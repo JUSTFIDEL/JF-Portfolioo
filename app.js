620656e5f5831d7ffebd965faff5eb3f6e0d4e28
JUSTFIDEL <fidenki1429@gmail.com>
2022-11-06 01:00:29 -0800

    Contact page added

--- app.js
+++ app.js
@@ -33,6 +33,16 @@ function pageTransition () {
             element.classList.add('active')
         }
     })
+
+    //Toggle theme
+
+    const themeBtn = document.querySelector('.theme-btn')
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
+
+
 }
 
 pageTransition()
